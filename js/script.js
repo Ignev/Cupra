1d38bdadc068c897f5209d1eaefb019ba5aa05e3
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-18 11:00:54 +0300

    start project

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//     // const moveBackground = () => {
+    
+//     // }
+//     // const swipe = () => {
+//     //     slide.addEventListener('mouse', ()=> {
+
+//     //     })
+//     // }
+    
+// })
+// slide.addEventListener('click', () => {
+//     let pos = slide.style.backgroundPositionX;
+//     slide.removeAttribute("style");
+//     pos -= 230;
+    
+// })
+// slide.addEventListener("click", () => {
+//     slide.style.backgroundPositionX = -230 + "px";
+//     if(slide.style.backgroundPositionX == "-230px")
+//     {
+//         slide.style.backgroundPositionX = -100 + "px";
+//     }
+    
+//     console.log(15);
+//   });
