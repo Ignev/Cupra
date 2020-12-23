a408a6a15fe78f2fd2c1b8c3e6265f224410e36e
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-23 11:31:55 +0300

    swipe

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
+let pos = 0;
+let wh = slide.offsetWidth;
+console.log(container.style.backgroundPositionX);
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+Hammer(slide).on('swiperight', (e)=>{
+    pos -= wh;
 
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+    container.style.backgroundPositionX = pos + "px";
+    console.log(typeof slide.offsetWidth , wh);
+    console.log(pos);
+    console.log(container.style.backgroundPositionX);
+})
+Hammer(slide).on('swipeleft', (e)=>{
+    pos += wh;
+    container.style.backgroundPositionX = 30 + "px";
+    console.log(typeof slide.offsetWidth , wh);
+    console.log(pos);
+    console.log(container.style.backgroundPositionX);
+})
+
+
+
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// window.addEventListener('resize', () => {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
 
 text__btn.addEventListener("click", () => {
   slider__popup.style.display = "flex";
