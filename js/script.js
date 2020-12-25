3f5b798c13d7aa0edaf9de3e7eeba382eefde342
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-25 14:13:37 +0300

    d

--- js/script.js
+++ js/script.js
@@ -73,7 +73,6 @@ if (typeof DeviceOrientationEvent === "function") {
     (e) => {
       let horPos = e.gamma;
         container.style.backgroundPositionX = Math.abs(e.gamma) + "%";
-        test.innerHTML = typeof e.gamma;
     },
     false
   );
