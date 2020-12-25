677322b169dc5bfac85766705a28157401f6ba79
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-25 15:13:39 +0300

    d

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ if (typeof DeviceOrientationEvent === "function") {
         container.style.backgroundPositionX = 0 + "%";
       }
       else{
-        container.style.backgroundPositionX = currentPositionBg + e.gamma + "%";
+        container.style.backgroundPositionX = currentPositionBg + e.gamma/10 + "%";
       }
     },
     false
@@ -94,7 +94,7 @@ if (typeof DeviceOrientationEvent === "function") {
         container.style.backgroundPositionX = 0 + "%";
       }
       else{
-        container.style.backgroundPositionX = currentPositionBg + e.gamma + "%";
+        container.style.backgroundPositionX = currentPositionBg + e.gamma/10 + "%";
       }
     },
     false
