9c01d29090988240d858db78d2dc447655d9eba5
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-28 19:17:39 +0300

    d

--- js/script.js
+++ js/script.js
@@ -53,12 +53,12 @@ popup__close.addEventListener("click", () => {
 
 let speed;
 if (!window.DeviceMotionEvent) {
-  test.innerHTML = "no ok 27";
+  test.innerHTML = "no ok 34";
 } else {
   window.addEventListener('devicemotion', function(event) {
     window.addEventListener('deviceorientation', function(e){
-      test.innerHTML = "ok 27";
-      let deg = e.gamma/500;
+      test.innerHTML = "ok 34";
+      let deg = e.gamma/10000;
       speed = event.rotationRate.gamma;
     if(speed < 0){
         currentPositionBg -= deg;
