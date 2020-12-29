8629e94c9918bb68630ffdbbf7ff1b58d922fa05
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2020-12-29 17:16:56 +0300

    d

--- js/script.js
+++ js/script.js
@@ -61,8 +61,8 @@ window.addEventListener("DOMContentLoaded", () => {
         window.addEventListener("deviceorientation", function (e) {
           let l = (sizeBgContainer * e.gamma)/180;
           
-          let deg = e.gamma/7;
-          test.innerHTML = "42 ";
+          let deg = e.gamma/15;
+          test.innerHTML = "43 ";
           speed = event.rotationRate.gamma;
           if (speed < 0) {
             currentPositionBg -= deg;
